e79f3b94c1bd6817641913f74340759529c9eedb
Ray Ye <yer@purdue.edu>
2021-01-07 11:01:29 -0500

    working api

--- app.js
+++ app.js
@@ -6,40 +6,31 @@ function formSubmit(e) {
 form.addEventListener('submit', formSubmit)
 
 async function locationInfo(location) {
-    /*try {
-        /*const response = await fetch(
-            'api.openweathermap.org/data/2.5/weather?q=${location}&appid=8fe71ca68a3b6fe511ff25541f586e61',
-            {
-              mode: 'cors',
-            }
-          )
-        reponse = {
-            city: 'new york',
-            weather: 30
-        }  
-        console.log(response.json())
-        response_object = response.json()
-        handleData(response_object)
-    }
+    try {
+        const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8fe71ca68a3b6fe511ff25541f586e61`,
+             {
+               mode: 'cors',
+              }
+             )
+        if (response.status == 400) {
+            console.log('oops')
+            return
+        }
+         let response_object = await response.json()
+         console.log(response_object)
+         handleData(response_object)
+    }   
     catch (error) {
         console.log('oops')
-    }*/
-    let response = {
-        location: {
-            name: 'new york'
-        },
-        current: {
-            temp_f: 40
-        }
-    }  
-    console.log(response)
-    response_object = response
-    handleData(response_object)
+    }
+    
+    
 }
 
 function handleData(weatherInfo) {
     let city = document.getElementById('city')
     let temp = document.getElementById('temp')
-    city.textContent = weatherInfo.location.name
-    temp.textContent = weatherInfo.current.temp_f
+    city.textContent = weatherInfo.name
+    temp.textContent = `${((weatherInfo.main.temp - 273.15) * 9/5 + 32).toFixed(2)} Farenheit`
 }
